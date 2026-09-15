ed167e8c95651ccd74f990342d6297410ed07929
Chris <115683479+fromis-9@users.noreply.github.com>
2026-09-15 18:16:43 -0400

    save

--- client/src/components/BannerDisplay.js
+++ client/src/components/BannerDisplay.js
@@ -64,6 +64,11 @@ function BannerDisplay({ data }) {
       <div className="banner-container">
         {images.map(image => <ArtworkImage key={image.imagePath} image={image} title={data.title} />)}
       </div>
+      <p className="artwork-support">
+        finding this useful?{' '}
+        <a href="https://www.buymeacoffee.com/corinthians" target="_blank" rel="noopener noreferrer">buy me a coffee</a>{' '}
+        to help cover hosting and banner extraction costs.
+      </p>
     </section>
   );
 }
